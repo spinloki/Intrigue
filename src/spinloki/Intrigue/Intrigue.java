9089cb1ec6cf3c0f5cde04c786040a7a570394fb
spinloki <spinloki997@gmail.com>
2026-02-23 15:17:06 -0700

    Set up settings helper

--- src/spinloki/Intrigue/Intrigue.java
+++ src/spinloki/Intrigue/Intrigue.java
@@ -1,21 +1,13 @@
 package spinloki.Intrigue;
 
 import com.fs.starfarer.api.BaseModPlugin;
+import spinloki.Intrigue.config.IntrigueSettings;
 
 public class Intrigue extends BaseModPlugin {
     @Override
     public void onApplicationLoad() throws Exception {
         super.onApplicationLoad();
 
-        // Test that the .jar is loaded and working, using the most obnoxious way possible.
-        throw new RuntimeException("Template mod loaded and working!\nRemove this crash in TemplateModPlugin.");
+        IntrigueSettings.loadSettingsFromJson();
     }
-
-    @Override
-    public void onNewGame() {
-        super.onNewGame();
-        // Add your code here, or delete this method (it does nothing unless you add code)
-    }
-
-    // You can add more methods from ModPlugin here. Press Control-O in IntelliJ to see options.
 }
---
+++ src/spinloki/Intrigue/config/IntrigueSettings.java
@@ -0,0 +1,14 @@
+package spinloki.Intrigue.config;
+
+import com.fs.starfarer.api.Global;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class IntrigueSettings {
+
+    public static void loadSettingsFromJson()  throws JSONException, IOException {
+
+    }
+}
